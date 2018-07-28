ab6dbdec935467a1783bf05d3a58e940acf60f7e
Jimmy <Jimmy@PC-JIMMY.home>
2018-07-28 09:21:06 +0200

    third commit

--- src/be/vdab/IndexServlet.java
+++ src/be/vdab/IndexServlet.java
@@ -1,6 +1,9 @@
 package be.vdab;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,7 +22,15 @@ public class IndexServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		out.println("<!doctype html>");
+		out.println("<html lang='nl'><head>");
+		out.println("<title>Pizza Luigi</title></head>");
+		out.println("<body><h1>");
+		int uur = LocalDateTime.now().getHour();
+		out.print(uur >= 6 && uur < 12 ? "Goede morgen" :
+		uur >= 12 && uur < 18 ? "Goede middag": "Goede avond");
+		out.println("</h1></body></html>");
 		
 	}
 
