0cd58878f90d0d6431e37a052610f01b344ecba4
Jimmy <Jimmy@PC-JIMMY.home>
2018-07-28 11:26:53 +0200

    jsp&servlets

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -4,14 +4,17 @@
 <%@page import="java.time.LocalDateTime"%>    
 <!DOCTYPE html>
 <html lang="nl">
-<head><title>Pizza Luigi</title></head>
+<head>
+	<title>Pizza Luigi</title>
+	<link rel='icon' href='images/favicon.ico'>
+	<meta name='viewport' content='width=device-width,initial-scale=1'>
+	<link rel='stylesheet' href='styles/default.css'>
+</head>
 <body>
 <h1>
-<% 
-int uur = LocalDateTime.now().getHour();
-out.print(uur >= 6 && uur < 12 ? "Goede morgen" :
-uur >= 12 && uur < 18 ? "Goede middag": "Goede avond");
-%>
+	Pizza Luigi
 </h1>
+<img src='images/pizza.jpg' alt='pizza' class='fullwidth'>
+<h2>${begroeting}</h2>
 </body>
 </html>
--- src/be/vdab/IndexServlet.java
+++ src/be/vdab/IndexServlet.java
@@ -1,8 +1,6 @@
 package be.vdab;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import javax.servlet.ServletException;
@@ -14,27 +12,18 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class IndexServlet
  */
-//@WebServlet("/index.htm")
+@WebServlet("/index.htm")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	private static final String VIEW = "index.jsp";
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		PrintWriter out = response.getWriter();
-		out.println("<!doctype html>");
-		out.println("<html lang='nl'><head>");
-		out.println("<title>Pizza Luigi</title></head>");
-		out.println("<body><h1>");
-		//int uur = LocalDateTime.now().getHour();
-		//out.print(uur >= 6 && uur < 12 ? "Goede morgen!!!" :
-		//uur >= 12 && uur < 18 ? "Goede middag": "Goede avond");
-		int dag = LocalDate.now().getDayOfWeek().getValue();
-		out.print(dag == 1 || dag == 4 ? "Vandaag zijn we gesloten!" :
-			"Vandaag zijn we open!");
-		out.println("</h1></body></html>");
+		int uur = LocalDateTime.now().getHour();
+		request.setAttribute("begroeting", uur >= 6 && uur < 12 ? "Goede morgen" :
+		uur >= 12 && uur < 18 ? "Goede middag": "Goede avond");
+		request.getRequestDispatcher(VIEW).forward(request, response);
 		
 		
 	}
