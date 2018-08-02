7956159378788413fb5c387372db0689279aeda4
Jimmy <Jimmy@PC-JIMMY.home>
2018-08-02 11:48:55 +0200

    MVC

--- src/be/vdab/servlets/PizzaServlet.java
+++ src/be/vdab/servlets/PizzaServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import be.vdab.entities.Pizza;
+import be.vdab.repositories.PizzaRepository;
 
 /**
  * Servlet implementation class PizzaServlet
@@ -23,9 +24,9 @@ public class PizzaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW = "/WEB-INF/JSP/pizzas.jsp";
 	private static final String PIZZAS_REQUESTS = "pizzasRequests";
+	private final PizzaRepository pizzaRepository = new PizzaRepository();
 
-	
-	
+		
 	@Override
 	public void init() throws ServletException {
 		this.getServletContext().setAttribute(PIZZAS_REQUESTS, new AtomicInteger());
@@ -38,12 +39,7 @@ public class PizzaServlet extends HttpServlet {
 //						new Pizza(14, "Margherita", BigDecimal.valueOf(5), false),
 //						new Pizza(17, "Calzone", BigDecimal.valueOf(4), false)));
 		((AtomicInteger) this.getServletContext().getAttribute(PIZZAS_REQUESTS)).incrementAndGet();
-		Map<Long, Pizza> pizzas = new LinkedHashMap<>(); // keys zijn pizza ids
-		pizzas.put(12L, new Pizza(12, "Prosciutto", BigDecimal.valueOf(4), true));
-		pizzas.put(14L, new Pizza(14, "Margehrita", BigDecimal.valueOf(5), false));
-		pizzas.put(17L, new Pizza(17, "Calzone", BigDecimal.valueOf(4), false));
-		pizzas.put(23L, new Pizza(23, "Fungi & Olive", BigDecimal.valueOf(5), false));
-		request.setAttribute("pizzas", pizzas);
+		request.setAttribute("pizzas", pizzaRepository.findAll());
 		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 
